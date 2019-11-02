a7532cc8fde845bff7b1534d1abb95bac1b10820
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2019-11-02 13:22:35 +0600

    added search option in venue list

--- app/src/main/java/brainstormapps/venuekoi/VenueListActivity.java
+++ app/src/main/java/brainstormapps/venuekoi/VenueListActivity.java
@@ -2,6 +2,8 @@ package brainstormapps.venuekoi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,9 +20,16 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import brainstormapps.venuekoi.Model.Venue;
 import brainstormapps.venuekoi.ViewHolder.VenueViewHolder;
@@ -32,6 +41,12 @@ public class VenueListActivity extends AppCompatActivity implements NavigationVi
     RecyclerView recyclerVenue;
     RecyclerView.LayoutManager layoutManager;
     FirebaseRecyclerAdapter<Venue, VenueViewHolder> adapter;
+
+    // search functionality
+    FirebaseRecyclerAdapter<Venue, VenueViewHolder> searchAdapter;
+    List<String> suggestList = new ArrayList<>();
+    MaterialSearchBar materialSearchBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +76,98 @@ public class VenueListActivity extends AppCompatActivity implements NavigationVi
         //Log.d("fimg", "item passed");
         loadVenueItem();
 
+        // search function
+        materialSearchBar = findViewById(R.id.venue_searchBar);
+        loadSuggestion();
+        materialSearchBar.setLastSuggestions(suggestList);
+        materialSearchBar.setCardViewElevation(10);
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                // when user type text , it will change suggestion list
+                List<String> suggest = new ArrayList<>();
+                for (String search:suggestList) {
+                    if (search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
+                        suggest.add(search);
+                }
+
+                materialSearchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                // when search bar is closed it will restore original adapter
+                if (!enabled)
+                    recyclerVenue.setAdapter(adapter);
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                // when search finished it will show search adapter
+                startSearch(text);
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+
+    }
+
+    private void startSearch(CharSequence text) {
+        searchAdapter = new FirebaseRecyclerAdapter<Venue, VenueViewHolder>(
+                Venue.class, R.layout.home_venue_item,
+                VenueViewHolder.class, venueItemRef.orderByChild("Name").equalTo(text.toString())) {
+            @Override
+            protected void populateViewHolder(VenueViewHolder venueViewHolder, Venue venue, int i) {
+
+                venueViewHolder.txtVenueName.setText(venue.getName());
+                venueViewHolder.txtVenuePrice.setText(venue.getPrice());
+                Picasso.get().load(venue.getImage()).into(venueViewHolder.imgVenue);
+
+                // send venue id to venue details activity for showing details
+                venueViewHolder.setItemClickListener((view, position, isLongClick) -> {
+                    Intent venueIntent = new Intent(VenueListActivity.this, VenueDetailsActivity.class);
+                    venueIntent.putExtra("VenueId", searchAdapter.getRef(position).getKey());
+                    startActivity(venueIntent);
+                });
+            }
+        };
+        recyclerVenue.setAdapter(searchAdapter); // set adapter for Recycler view in search result
+    }
+
+    private void loadSuggestion() {
+
+        venueItemRef.orderByChild("Name").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot postDataSnapshot:dataSnapshot.getChildren()) {
+                    Venue venueItem = postDataSnapshot.getValue(Venue.class);
+                    suggestList.add(venueItem.getName()); // add name of venue to suggest list
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void loadVenueItem() {
